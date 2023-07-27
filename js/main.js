95537942bc849316191945270410f71ed5ba5109
leonardo.nascimento <leonardo.nascimento@lwtecnologia.com.br>
2023-07-27 13:34:32 -0300

    refactor

--- js/main.js
+++ js/main.js
@@ -1,15 +1,12 @@
 import { orgaosGerais } from "../json/orgaosParaVerificar.js";
 import { todosDetalhes } from "../json/detalhes.js";
-
 import identificaOrgaoEImprimeDetalhes from "./identificaOrgao.js";
 import fechaAbaDetalhes from "./fechaAbaDetalhes.js";
 import criarLista from "./criarLista.js";
 import pesquisaOrgao from "./pesquisarOrgao.js";
 import renderizaOrgaos from "./renderizaOrgaos.js";
-//import pesquisaOrgao from "./pesquisarOrgao.js";
 
 const table = document.getElementById("tabela");
-const estados = document.querySelectorAll("a");
 const tabelaGeral = document.getElementById("tabelaGeral");
 const titulos = {
   coluna1: "Órgão",
@@ -17,8 +14,7 @@ const titulos = {
   coluna3: "Link",
   coluna4: "Detalhes",
 };
-// TITULOS DA TABELA -------------------
-//Cria os headers da tabela
+
 function tableHeader() {
   return `
 <tr>
@@ -30,23 +26,20 @@ function tableHeader() {
 <tr>
 `;
 }
+
 table.innerHTML = tableHeader();
 tabelaGeral.innerHTML = tableHeader();
-criarLista(orgaosGerais, tabelaGeral);
-// ---------------- EVENTOS AO CLICAR EM UM ESTADO DO MAPA
-renderizaOrgaos();
-//------------------------- EVENTO AO PESQUISAR UM ÓRGÃO-----------------------
-pesquisaOrgao();
-// ---------------------FUNCIONALIDADE 'DETALHES' ----------------------------
 
-//Quando o botão de 'detalhes' for clicado, verifica qual é o órgão correspondente
+criarLista(orgaosGerais, tabelaGeral);
+renderizaOrgaos();
+pesquisaOrgao();
+
 table.addEventListener("click", (e) => {
   identificaOrgaoEImprimeDetalhes(e, todosDetalhes);
-
   fechaAbaDetalhes();
 });
+
 tabelaGeral.addEventListener("click", (e) => {
   identificaOrgaoEImprimeDetalhes(e, todosDetalhes);
-
   fechaAbaDetalhes();
 });
